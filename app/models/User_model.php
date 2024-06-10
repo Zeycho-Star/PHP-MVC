13fd020661610e0b3fc0ea6a0807575649082e2f
unknown <darmagans28@gmail.com>
2024-06-10 15:13:27 +0700

    first commit

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Darma';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+<div class="jumbotron mt-4">
+  <h1 class="display-4">WELCOME TO MY WEBSITE</h1>
+  <p class="lead">Hello, my name is <?= $data ['nama'];?></p>
+  <hr class="my-4">
+  <p>PHP MVC material Pemograman Berorientasi Objek lesson taught by Mrs. linda.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+
+</div>
